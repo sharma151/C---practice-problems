a6d6f23c001635319905712ef9c53cfef7a43651
sharma151 <sharmasaurav150503@gmail.com>
2022-08-25 00:44:49 +0530

    program20 added

--- program20.cpp
+++ program20.cpp
@@ -1,1 +1,19 @@
 /*Write a C++ Program To Find Character Is Vowel Or Not Using If/Else Statements */
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "enter a character";
+    cin >> &c;
+    if (c == 'a' || c == 'e' || c == 'i' || c == '0' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+    {
+        cout << " entered character is vowel ";
+    }
+    else
+    {
+        cout << "it is a consonent";
+    }
+
+    return (0);
+}
